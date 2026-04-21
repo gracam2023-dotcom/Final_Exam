7f29ca8bd072dc3d01447e972cb36a05a5213945
gracam2023-dotcom <gracam2023@gmail.com>
2026-04-21 12:47:28 -0400

    Update loanapp.py

--- loanapp.py
+++ loanapp.py
@@ -7,7 +7,7 @@ import sklearn  # This is needed for the pickle file to load!
 
 # Load the trained model
 # --- Put the Model in Drive First---
-with open(my_model.pkl", "rb") as file:
+with open("my_model.pkl", "rb") as file:
     model = pickle.load(file)
 
 # Title for the app
